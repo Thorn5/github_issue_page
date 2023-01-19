6b5e01cf2c0bb3e0d33a7bc16dd01e2afcadde27
Trygve Horn <trygvehorn@gmail.com>
2023-01-19 20:59:27 +0100

    ss

--- src/Components/OpenIssues.js
+++ src/Components/OpenIssues.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-const[date, setData] = useState ([])
-useEffect(() => {
-    const OpenIssues = () => {
+const [data, setData] = useState([])
+
+const OpenIssues = () => {
+    useEffect(() => {
         fetch("https://api.github.com/repos/rails/rails/issues?page=1")
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => {console.error('Error:', error)});
-
-        return(
-            <h1> Hi there</h1>
-            {data.lenght}
-        )
-},[])
-
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => { console.error('Error:', error) });
+    }, [])
+    
+    return (
+        <h1> Hi there</h1>
+        { data.length }
+    )
+}
 export default OpenIssues;
